75d2d40e29631ae371a9441ca213a73c6e0152db
hectoruptpc <programador_control_estudios@uptpc.edu.ve>
2025-11-04 13:37:50 -0400

    cambio de imagen y mejora en la descarga de la imagen

--- index.php
+++ index.php
@@ -589,7 +589,7 @@ if (isset($_SESSION['cedula'])) {
                             <h5 class="card-title text-primary">5. Análisis Forense</h5>
                             <h6 class="card-subtitle mb-2 text-muted">Metadatos EXIF (1 🚩)</h6>
                             <p class="card-text">Descarga la imagen y analiza sus metadatos EXIF para encontrar la bandera oculta.</p>
-                            <p class="fw-bold">Imagen: <a href="mystery_image.jpeg">mystery_image.jpeg</a></p>
+                            <p class="fw-bold">Imagen: <a href="mystery_image.jpeg" download>mystery_image.jpeg</a></p>
                             
                             <div class="mt-3">
                                 <input type="text" class="form-control" id="flag-meta" placeholder="Ingresa la bandera">
